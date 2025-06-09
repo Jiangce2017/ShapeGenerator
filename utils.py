e615d55c16ded80bc784f0b533288ab16dffc927
Achez <achez@10-18-128-125.dynapool.wireless.nyu.edu>
2025-06-09 15:19:16 -0400

    mat load function

--- utils.py
+++ utils.py
@@ -17,4 +17,8 @@ def loss_function(x, x_hat, mean, log_var):
     
     
 def load_mat(filename):
-### write a load_mat code here (you can ask an LLM)
+    with h5py.File(filename, 'r') as f:
+        data = {}
+        for k, v in f.items():
+            data[k] = v[:]  # Load data into memory
+    return data
