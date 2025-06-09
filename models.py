05cac5a63260ccce935aeec119d56157f449db1f
Achez <achez@10-18-128-125.dynapool.wireless.nyu.edu>
2025-06-09 15:27:45 -0400

    Task for FNO VAE

--- models.py
+++ models.py
@@ -11,6 +11,9 @@ class Model(nn.Module):
         elif model_type == 'FL':
             self.Encoder = FL_Encoder(input_dim=x_dim, hidden_dim=hidden_dim, latent_dim=latent_dim)
             self.Decoder = FL_Decoder(latent_dim=latent_dim, hidden_dim = hidden_dim, output_dim = x_dim)
+        elif model_type == 'FNO':
+            self.Encoder = FNO_Encoder(input_dim=x_dim, hidden_dim=hidden_dim, latent_dim=latent_dim)
+            self.Decoder = FNO_Decoder(latent_dim=latent_dim, hidden_dim = hidden_dim, output_dim = x_dim)
         
     def reparameterization(self, mean, var):
         epsilon = torch.randn_like(var).to(self.device)        # sampling epsilon        
@@ -118,61 +121,22 @@ class CNN_Decoder(nn.Module):
         x_hat = self.sigmoid(self.conv2(h))
         return x_hat   
 
-class FNO_Encoder(nn.Module):
-        def __init__(self, input_dim, hidden_dim, latent_dim,im_x,im_y):
-            super(FNO_Encoder, self).__init__()
-            self.conv1 = nn.Conv2d(input_dim, hidden_dim, kernel_size=(3, 3), stride=1, padding=1)
-            self.conv2 = nn.Conv2d(hidden_dim, hidden_dim, kernel_size=(3, 3), stride=1, padding=1)
-            self.maxpool = nn.MaxPool2d(kernel_size=(2, 2)) ## half spatial dimension 
-            self.conv3 = nn.Conv2d(hidden_dim, hidden_dim*2, kernel_size=(3, 3), stride=1, padding=1)
-            self.conv4 = nn.Conv2d(hidden_dim*2, hidden_dim*2, kernel_size=(3, 3), stride=1, padding=1)
-            self.conv5 = nn.Conv2d(hidden_dim*2, hidden_dim*2, kernel_size=(3, 3), stride=1, padding=1)
-            self.flatten = nn.Flatten()
-            self.dense1 = nn.Linear(im_x*im_y*16, hidden_dim)
-            self.layer_mean = nn.Linear(hidden_dim, latent_dim)
-            self.layer_variance = nn.Linear(hidden_dim, latent_dim)
-            self.LeakyReLU = nn.LeakyReLU(0.2)
-            self.im_x = im_x
-            self.im_y = im_y
+# class FNO_Encoder(nn.Module):
+#         def __init__(self, input_dim, hidden_dim, latent_dim,im_x,im_y):
+#             super(FNO_Encoder, self).__init__()
+
+
             
-        def forward(self, x):
-            x = x.view(-1,1,self.im_x,self.im_y)
-            h_ = self.LeakyReLU(self.conv1(x))
-            h_ = self.LeakyReLU(self.conv2(h_))
-            h_ = self.maxpool(h_)
-            h_ = self.LeakyReLU(self.conv3(h_))
-            h_ = self.LeakyReLU(self.conv4(h_))
-            h_ = self.LeakyReLU(self.conv5(h_))
-            h_ = self.flatten(h_)
-            h_ = self.LeakyReLU(self.dense1(h_))
-            mean     = self.layer_mean(h_)
-            log_var  = self.layer_variance(h_)                     # encoder produces mean and log of variance 
-                                                        #             (i.e., parateters of simple tractable normal distribution "q"
-            return mean, log_var
+#         def forward(self, x):
+#             x = x.view(-1,1,self.im_x,self.im_y)
+
+#             return mean, log_var
     
-class FNO_Decoder(nn.Module):
-    def __init__(self, latent_dim, hidden_dim, output_dim,im_x,im_y):
-        super(FNO_Decoder, self).__init__()
-
-        self.dense1 = nn.Linear(latent_dim, im_x*im_y*2)
-        self.dense2 = nn.Linear(im_x*im_y*2,im_x*im_y*16)
-
-        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear')
-        self.conv1 = nn.Conv2d(hidden_dim*2, hidden_dim, kernel_size=(3, 3), stride=1, padding=1)
-        self.conv2 = nn.Conv2d(hidden_dim, 1, kernel_size=(3, 3), stride=1, padding=1)
-
-        self.LeakyReLU = nn.LeakyReLU(0.2)
-        self.sigmoid = nn.Sigmoid()
-        self.hidden_dim = hidden_dim
-        self.im_x = im_x
-        self.im_y = im_y
+# class FNO_Decoder(nn.Module):
+#     def __init__(self, latent_dim, hidden_dim, output_dim,im_x,im_y):
+#         super(FNO_Decoder, self).__init__()
 
         
-    def forward(self, x):
-        h = self.LeakyReLU(self.dense1(x))
-        h = self.LeakyReLU(self.dense2(h))
-        h = h.view(-1,self.hidden_dim*2,self.im_x//2,self.im_y//2)
-        h = self.upsample(h)
-        h = self.LeakyReLU(self.conv1(h))
-        x_hat = self.sigmoid(self.conv2(h))
-        return x_hat    
+#     def forward(self, x):
+
+#         return x_hat    
--- train.py
+++ train.py
@@ -42,8 +42,9 @@ if __name__ == '__main__':
     dataset = mat_data['ShapeSpace']
     dataset = dataset.astype(np.float32)
     dataset = dataset[:256]
-
-    ### load training and testing data from dataset, testing data account for 10% (asking an LLM to help you)
+    train_dataset, test_dataset = train_test_split(dataset, test_size=0.1, random_state=42)
+    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True,drop_last=True, **kwargs)
+    test_loader  = DataLoader(dataset=test_dataset,  batch_size=batch_size, shuffle=False,drop_last=False, **kwargs)
         
     model = Model(x_dim, hidden_dim, latent_dim,device,model_type,im_x,im_y).to(device)
 
