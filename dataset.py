025b6ca0925b2282a37516c34be45e930f95e530
Jiangce Chen <jc14407@log-1.hpc.nyu.edu>
2025-07-31 11:43:08 -0400

    add dataset generator

---
+++ dataset.py
@@ -0,0 +1,45 @@
+import os
+from torch.utils.data import Dataset
+from utils import voxelize_stl
+import torch
+import trimesh
+import numpy as np
+
+class ABCDataset(Dataset):
+    def __init__(self, root_dir, size=10000, voxelized = False):
+        """
+        Args:
+            root_dir (str): Folder containing .stl files
+        """
+        self.root_dir = root_dir
+        self.voxelized = voxelized
+        self.voxels = []
+        if voxelized:
+            self.voxels = [
+                np.load(os.path.join(root_dir, f))
+                for f in os.listdir(root_dir)
+                if f.endswith('.npy')
+            ]
+        else:
+            self.modelPaths = [
+                os.path.join(root_dir, f)
+                for f in os.listdir(root_dir)
+                if f.endswith('.stl')
+            ][:size]
+
+    def __len__(self):
+        if self.voxelized:
+            return len(self.voxels)
+        return len(self.modelPaths)
+
+    def __getitem__(self, idx):
+        if self.voxelized:
+            vox = self.voxels[idx]
+            vox_tensor = torch.tensor(vox, dtype=torch.float32).unsqueeze(-1)
+        else:
+            path = self.modelPaths[idx]
+            mesh = trimesh.load_mesh(path)
+            vox = voxelize_stl(path)  # shape: [D, H, W] numpy array
+            vox_tensor = torch.tensor(vox, dtype=torch.float32).unsqueeze(-1)  # → [D, H, W, 1]
+            print(f"Item voxelized with {len(mesh.faces)} faces")
+        return  vox_tensor
